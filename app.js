e01abdbfecbd9e97ca0e0df93f20063c1f9b0830
Junaidahmedjs1 <junaidahmed31254@gmail.com>
2024-09-15 19:04:41 +0500

    Chap-7 Conditions Complete

---
+++ app.js
@@ -0,0 +1,108 @@
+// CHAPTER-7 CONDITIONS
+
+// Answer No 1
+
+// var number = 15
+// if(number % 3 ==0){
+//     console.log("The Number is Divide By 3");
+// }
+// else{
+//     console.log("The Number is Not Divide By 3");
+        
+// }
+
+
+// Answer No 2 
+
+// var number = 87
+// if(number % 2 ==0){
+//     console.log("The Number is Even");
+// }
+// else{
+//     console.log("The Number is Odd");
+        
+// }
+
+// Answer No 3
+
+
+// var age = 17
+// if(age>=18){
+//     console.log("Old enough");
+// }
+// else{
+//     console.log("Too Young");
+// }
+
+
+// Answer No 4
+
+
+// var name=prompt("Enter Your Name")
+// if(name==="Junaid"){
+//     console.log("You Are Become A fastest Web Developer Inshallah");
+// }
+// else{
+//     console.log("invalid User");
+// }
+
+
+// Answer No 6
+
+
+// var letter=+prompt("Enter number");
+
+// if(letter>=65 && letter<=91){
+//     console.log("The letter is Uppercase")
+// }
+// else if(letter>=97 && letter<=123){
+//     console.log("The letter is lowerCase")
+// }
+// else{
+//     console.log("invalid letter")
+// }
+
+
+// Answer No 8
+
+// var time = +prompt("Enter time")
+// if(time >0 && time<=1200){
+//     console.log("Good Morning");
+    
+// }
+// else if(time >1300 && time<=1700) {
+//     console.log("Good AfterNoon");
+// }
+// else if(time >1800 && time<=2100){
+//     console.log("Good Evening");
+    
+// }
+// else if(time>2200 && time<=2350){
+//     console.log("Good Night");
+    
+// }
+// else{
+//     console.log("invalid Time")
+// }
+
+
+// Answer No 10
+
+
+// var password = "Jun@id20"
+// var enterPassword = prompt("Enter Your Password")
+
+// if(enterPassword===password){
+//     console.log("You are Enter a correct Password")
+
+// }
+
+// else{
+//     console.log("Plss! Enter a Correct Password");
+
+// }
+
+
+
+
+
